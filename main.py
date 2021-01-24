147c70fbb33a1e7bf276565ff26892edfff66fbb
Shan <pruskimateusz93@gmail.com>
2021-01-24 17:10:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# DUMMY PROJECT
+# Create script that:
+# 1. Captures continously text from screen (best case - only when it changes or on demand)
+# 2. Does OCR
+# 3. Runs text and its fragments through google translate
+# 3a. Bonus points - translates only the more obscure words
+# 4. Uses some kind of GUI for this
