d6b0c6d8d0117a8f3d492e1bc90f7a3b5df1cec9
Shan <pruskimateusz93@gmail.com>
2021-02-27 13:07:34 +0100

    Last entry with this file, will be deleted shortly.

--- automated_picture_translator.py
+++ automated_picture_translator.py
@@ -4,52 +4,31 @@
 
 from __future__ import annotations
 
-import cv2
-import numpy as np
-
-import re
 import time
 
 import pyautogui
 
-from typing import NamedTuple, List
+from typing import NamedTuple
 
 import pytesseract
 from PIL import Image
 
-from googletrans import Translator
-
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+from picture_processing import process_picture_ocr, white_to_black_only
+from translation_processing import translate_text, translate_all_words, get_single_words_to_translate
 
 Base = declarative_base()
 # TODO to be removed after adding Tesseract to PATH
 pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract-OCR\tesseract.exe'
 
-translator = Translator()
 engine = create_engine("sqlite:///translation.db", echo=False)
 session = sessionmaker(bind=engine)()
-punctuation_regex = re.compile("[.,!:;]")
 
 
 # TODO In general - move everything to class and modulize what is needed
-
-def pil_to_cv2(image):
-    return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-
-
-def white_to_black_only(image):
-    arr = pil_to_cv2(image)
-    return Image.fromarray(np.where(arr == 255, 0, 255).astype('uint8'))
-
-class Translations(Base):
-    __tablename__ = 'translations'
-    translation_id = Column(Integer, primary_key=True)
-    source_language = Column(String, nullable=False)
-    target_language = Column(String, nullable=False)
-    text_to_translate = Column(String, nullable=False)
-    translation = Column(String, nullable=False)
 
 
 class ScreenshotSize(NamedTuple):
@@ -70,34 +49,12 @@ class ScreenPoint(NamedTuple):
         )
 
 
-def cache_translation(text_to_translate, source_language, target_language, translation):
-    translation = Translations(source_language=source_language, target_language=target_language,
-                               text_to_translate=text_to_translate, translation=translation)
-    session.add(translation)
-    session.commit()
-
-
-def translate_or_get_from_cache(text_to_translate, source_language, target_language):
-    entry: Translations = session.query(Translations).filter(
-        Translations.source_language.is_(source_language),
-        Translations.text_to_translate.is_(text_to_translate),
-        Translations.target_language.is_(target_language)
-    ).first()
-    if not entry:
-        try:
-            translator_response = translator.translate(text_to_translate, src=source_language, dest=target_language)
-            cache_translation(text_to_translate, source_language, target_language, translator_response.text)
-            return translator_response.text
-        except TypeError as err:
-            print(f"Error processing text: {text_to_translate}")
-            print(f"Error: {err}")
-            return ""
-    return entry.translation
-
-
 def get_corners_of_region_to_translate():
     # TODO select screenshot points somehow (UI?)
-    return ScreenPoint(x=743, y=890), ScreenPoint(x=1603, y=1139)
+    # return ScreenPoint(x=743, y=890), ScreenPoint(x=1603, y=1139)
+    # return ScreenPoint(x=987,y=408), ScreenPoint(x=2128,y=639)
+    #return ScreenPoint(x=693, y=981), ScreenPoint(x=1631,y=1197)
+    return ScreenPoint(x=513,y=1016), ScreenPoint(x=1814,y=1293)
 
 
 def capture_picture(top_left_corner: ScreenPoint, bottom_right_corner: ScreenPoint) -> Image:
@@ -105,30 +62,6 @@ def capture_picture(top_left_corner: ScreenPoint, bottom_right_corner: ScreenPoi
     print(f"Capturing picture from {top_left_corner} of size {size}")
     time.sleep(2)
     return pyautogui.screenshot(region=(*top_left_corner, *size))
-
-
-def process_picture_ocr(picture: Image) -> str:
-    custom_config = r'-l eng --psm 6'
-    print(f"Picture to OCR: {picture}")
-    raw_text = pytesseract.image_to_string(picture, config=custom_config)
-    return raw_text.strip().replace('\n', ' ')
-
-
-def translate_text(text_to_translate: str, source_language: str, target_language: str) -> str:
-    # TODO use Google Translate API for this one
-    print(f"Translating {text_to_translate} from {source_language} to {target_language}")
-    translated_text = translate_or_get_from_cache(text_to_translate, source_language, target_language)
-    return translated_text
-
-
-def translate_all_words(text_to_translate: str, source_language: str, target_language: str) -> List[str]:
-    words = text_to_translate.split(' ')
-    translations = []
-    for word in words:
-        text = translate_or_get_from_cache(punctuation_regex.sub("", word), source_language=source_language,
-                                           target_language=target_language)
-        translations.append(text)
-    return translations
 
 
 def display_translation(translated_text: str) -> None:
@@ -140,8 +73,9 @@ def main() -> None:
     top_left, bottom_right = get_corners_of_region_to_translate()
     Base.metadata.create_all(engine)
     session.commit()
+    time_between_checks = 5
     while True:
-        time.sleep(2)
+        time.sleep(time_between_checks)
         picture = capture_picture(top_left, bottom_right)
         text_to_translate = process_picture_ocr(white_to_black_only(picture))
         if not text_to_translate:
@@ -149,7 +83,8 @@ def main() -> None:
         if not text_to_translate:
             continue
         translated_text = translate_text(text_to_translate, 'en', 'pl')
-        print(translate_all_words(text_to_translate, 'en', 'pl'))
+        words = get_single_words_to_translate(text_to_translate)
+        print(translate_all_words(words, 'en', 'pl'))
         display_translation(translated_text)
 
 
