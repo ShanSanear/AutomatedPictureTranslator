e7640fd5da10564d0bb9e435387ede9a641f2ef8
Shan <pruskimateusz93@gmail.com>
2021-09-22 20:52:20 +0200

    Created exception. Removed position as parameter

--- automated_picture_translator_api.py
+++ automated_picture_translator_api.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 import translation_processing
@@ -24,17 +24,14 @@ async def read_root():
     return {"Hello": "World!"}
 
 
-@app.get("/translate/{position}")
+@app.get("/translate")
 async def translate_screen_part(position: CapturePosition):
     pic = picture_processing.capture_picture(top_left=position.top_left, size=position.size)
     sentence = picture_processing.process_picture_white_to_black(pic)
     if not sentence:
         sentence = picture_processing.process_picture_ocr(pic)
     if not sentence:
-        return Translation(sentence="ERROR",
-                           sentence_words=[""],
-                           translated_sentence="ERROR",
-                           translated_words=[""], )
+        raise HTTPException(status_code=404, detail="Couldn't translate right now")
     translated_sentence = translation_processing.translate_text(sentence, source_language='en', target_language='pl')
     sentence_words = translation_processing.get_single_words_to_translate(sentence)
     translated_words = translation_processing.translate_all_words(sentence_words, source_language='en',
